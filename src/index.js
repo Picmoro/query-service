456b72c0a8a71113c49682cc265a9edbc094158b
Jaime Zamora <kratov@protonmail.com>
2021-08-29 22:24:29 -0500

    Added event moderation Update

--- src/index.js
+++ src/index.js
@@ -21,9 +21,20 @@ app.post('/events', async (req, res) => {
         }
         case "CommentCreated":
         {
-            const {id, content, postId} = data;
+            const {id, content, postId, status} = data;
             const post = posts[postId];
-            post.comments.push({id,  content})
+            post.comments.push({id,  content, status})
+            break;
+        }
+        case "CommentUpdated":
+        {
+            const {postId, id, status, content} = data;
+            const post = posts[postId];
+            const comment = post.comments.find(comment => {
+                return comment.id === id
+            })
+            comment.status = status;
+            comment.content = content;
             break;
         }
     }
